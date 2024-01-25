1527718b1017b38d56602c62c2cd97c632aed621
alex <alexandre.longeaud08@gmail.com>
2024-01-25 11:37:50 +0100

    initialisation des assets avec bootstrap + ajout navbar

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,24 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+// assets/app.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'bootstrap';
+
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+//Ajout d'un écouteur d'événement sur le document
+//Au moment ou le document est complément charger, les fonction à l'intérieur vont être charger
+document.addEventListener('DOMContentLoaded', () => {
+    enableDropdowns();
+});
+
+// Fonction js pour faire fonctionner les menus dropdown. 
+// Js provenant de la doc bootstrap 
+// @see:https://getbootstrap.com/docs/5.3/components/dropdowns/#overview
+const enableDropdowns = () =>
+{
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+    const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+}
+
