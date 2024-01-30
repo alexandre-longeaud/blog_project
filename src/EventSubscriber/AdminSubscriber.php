72fb3b55153c434ec9a0f7bbd153b4326aecec05
alex <alexandre.longeaud08@gmail.com>
2024-01-30 11:15:27 +0100

    ajout des articles

--- src/EventSubscriber/AdminSubscriber.php
+++ src/EventSubscriber/AdminSubscriber.php
@@ -2,15 +2,42 @@
 
 namespace App\EventSubscriber;
 
-
+use App\Model\TimestampedInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class AdminSubscriber implements EventSubscriberInterface
 {
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents():array
     {
         return [
-            
-        ]
+            BeforeEntityPersistedEvent::class=>['setEntityCreatedAt'],
+            BeforeEntityUpdatedEvent::class=>['setEntityUpdatedAt'],
+        ];
+    }
+
+    public function setEntityCreatedAt(BeforeEntityPersistedEvent $event):void
+    {
+        $entity = $event->getEntityInstance();
+        // Condition : si $entity n'est pas une instance de TimestampedInterface, on retourne rien.
+        
+        if(!$entity instanceof TimestampedInterface){
+            return;
+        }
+        // Sinon : on set createdAt pour avoir la date de création de l'article 
+        $entity->setCreatedAt(new \DateTime());
+    }
+
+    public function setEntityUpdatedAt(BeforeEntityUpdatedEvent $event):void
+    {
+        $entity = $event->getEntityInstance();
+        // Condition : si $entity n'est pas une instance de TimestampedInterface, on retourne rien.
+        
+        if(!$entity instanceof TimestampedInterface){
+            return;
+        }
+        // Sinon : on set updatedAt pour avoir la date de mise à jour de l'article 
+        $entity->setUpdatedAt(new \DateTime());
     }
 }
---
+++ src/Model/TimestampedInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model;
+
+use DateTimeInterface;
+
+interface TimestampedInterface
+{
+
+    public function getCreatedAt(): ?DateTimeInterface;
+
+    public function setCreatedAt(\DateTimeInterface $createdAt);
+
+    public function getUpdatedAt(): ?DateTimeInterface;
+    
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt);
+
+}
